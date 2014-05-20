37860d570e1378120af8971e59907e7507783718
Coneptum <coneptum@coneptum-VirtualBox.(none)>
2014-05-20 15:41:58 +0200

    email unique

--- segment/forms.py
+++ segment/forms.py
@@ -64,6 +64,13 @@ class UserCreateForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
+
 
 class PassworResetForm(forms.Form):
     error_messages = {
